716f44b57979bec3bc5228cf43149beabc8e9b8f
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-10-21 22:17:03 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+export default function App() {
+  const { t, i18n } = useTranslation();
+
+  const handleChangeLng = (lng) => {
+    i18n.changeLanguage(lng);
+    localStorage.setItem("lng", lng);
+  };
+
+  console.log(i18n.language);
+
+  return (
+    <div>
+      <button onClick={() => handleChangeLng("en")}>English</button>
+      <button onClick={() => handleChangeLng("jp")}>Jp</button>
+
+      <h1>{t("hello")}</h1>
+      <h1>{t("how are you?")}</h1>
+    </div>
+  );
+}
